aa963fbfd9f3cdb100673c5f5a95ae1155bd6392
simonaspalt <simaspa@gmail.com>
2024-03-28 15:59:47 +0200

    up to ubmit firt info on ad

--- src/test/java/TestClass1.java
+++ src/test/java/TestClass1.java
@@ -3,90 +3,115 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 public class TestClass1 {
+//    @BeforeTest
+//    public  void WebDriverSetUp(){
+//        _globalDriver = new ChromeDriver();
+//    }
+
+
+
+     @Test
+    public void testUploadNewAd(){
+          ChromeDriver driver = new ChromeDriver();
+
+          driver.get("https://elenta.lt");
+          WebElement cookieButton = driver.findElement(By.xpath("/html/body/div[5]/div[2]/div[1]/div[2]/div[2]/button[1]/p"));
+          cookieButton.click();
+
+          WebElement loginButton = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div/a[3]"));
+          loginButton.click();
+
+          driver.findElement(By.id("UserName")).sendKeys("belekoks");
+          driver.findElement(By.id("Password")).sendKeys("Belekoks@23");
+          driver.findElement(By.xpath("/html/body/div[1]/form/fieldset/table/tbody/tr[4]/td[2]/input")).click();
+
+          driver.findElement(By.id("submit-new-ad-nav-button")).click();
+
+          WebElement cattegory = driver.findElement(By.xpath("/html/body/div[1]/ul/li[1]/a"));
+          cattegory.click();
+
+          WebElement subCattegory = driver.findElement(By.xpath("/html/body/div[1]/ul/li[1]/a"));
+          subCattegory.click();
+
+          driver.findElement(By.id("title")).sendKeys(generateUserName());
+          driver.findElement(By.id("text")).sendKeys(generateUserName() + " " + generateUserName() + " " + generateUserName() + generateUserName());
+          driver.findElement(By.id("price")).sendKeys("15");
+          driver.findElement(By.id("location-search-box")).sendKeys("Vilnius");
+          driver.findElement(By.id("phone")).sendKeys("+370666684666");
+          driver.findElement(By.id("email")).sendKeys(generateRandomEmail());
+
+          driver.findElement(By.id("submit-button")).click(); }
+
+
+
+          try {
+        TimeUnit.SECONDS.sleep(5);
+    }  catch (InterruptedException e){
+               e.printStackTrace();
+    }
+
+
+
     @Test
-    public void atidarytiNarsykle() {
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://www.google.lt");
-        Assert.assertEquals("f", "y");
-    }
-
-    @Test // anotacija, kuri leidžia javai suprasti, kad tai yra testas ir jį reikia paleisti.
-    public void test1() {//jei atidaro teisingai pakūrėme projektą
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.skelbiu.lt");//reikia https
-    }
-
-    @Test //leisti po viena
-    public void test2() {//jei atidaro teisingai pakūrėme projektą
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.skelbiu.lt");
-
-        WebElement cookieButton = driver.findElement(By.xpath("/html/body/div[9]/div[2]/div/div[1]/div/div[2]/div/button[1]")); //čia pilnas xphath. parodyti kaip išsitraukti pilną ir dalinį. (//*[@id="onetrust-accept-btn-handler"]) papasakoti kuo skirias. Dalinis xpath eina nuo artimiausio tėvinio unikalaus švyturio, pilnas nuo HTML pradžios.
-        cookieButton.click();
-        driver.findElement(By.xpath("//*[@id=\"searchKeyword\"]")).sendKeys("traktorius");// operacija nekuriant kintamojo.
-        driver.findElement(By.xpath("//*[@id=\"searchButton\"]")).click();
-    }
-
-    @Test //leisti po viena
-    public void test3() {//jei atidaro teisingai pakūrėme projektą
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.skelbiu.lt");
-        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
-        driver.findElement(By.id("searchKeyword")).sendKeys("traktorius");// operacija nekuriant kintamojo.
-        // //*[@id="searchKeyword"]  paltginti dalinio xpath struktūrą su id. iš xpath matosi, kad elementas turi savo id.
-        // papasakoti kad galima selectinti pagal id, unikalu, pagal klase, (jei vienas elementas ok, jei keli su ta klase paims pirmą. arba su findelements galima paimti visus su x klase, bet tai vėliau.)
-        driver.findElement(By.id("searchButton")).click();
-    }
-
-
-    @Test //leisti po viena
     public void testELentosregistration() {//jei atidaro teising
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://elenta.lt/registracija");
-        WebElement cookieButton = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div[1]/div[2]/div[2]/button[1]/p"));
-        cookieButton.click();
+         ChromeDriver driver = new ChromeDriver();
+
+         driver.get("https://elenta.lt");
+         WebElement cookieButton = driver.findElement(By.xpath("/html/body/div[5]/div[2]/div[1]/div[2]/div[2]/button[1]/p"));
+         cookieButton.click();
+
+        WebElement login = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div/a[3]"));
+        login.click();
+
+        WebElement register = driver.findElement(By.xpath("/html/body/div[1]/form/fieldset/table/tbody/tr[10]/td/p/a"));
+        register.click();
+
         driver.findElement(By.id("UserName")).sendKeys(generateUserName());
         driver.findElement(By.id("Email")).sendKeys(generateRandomEmail());
-        driver.findElement(By.id("Password")).sendKeys("");
-        driver.findElement(By.id("Password2")).sendKeys("");
+        driver.findElement(By.id("Password")).sendKeys("Belekoks@23");
+        driver.findElement(By.id("Password2")).sendKeys("Belekoks@23");
         String registruotis = new String ("/html/body/div[1]/form/fieldset/table/tbody/tr[11]/td[2]/input");
         WebElement registration = driver.findElement(By.xpath(registruotis));
         registration.click();
+        WebElement valifdationText = driver.findElement(By.xpath("/html/body/div[1]/div[2]/h1/b"));
+        Assert.assertEquals( valifdationText.getText(), "Jūs sėkmingai prisiregistravote!");
         
-        
+
+
     }
     public static String generateRandomEmail() {
             String[] domains = {"gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "example.com"};
             String[] characters = {"abcdefghijklmnopqrstuvwxyz", "0123456789"};
-            
+
             Random random = new Random();
-            
+
             StringBuilder email = new StringBuilder();
-            
+
             // Generate username part
             int usernameLength = random.nextInt(10) + 5; // Random length between 5 to 14 characters
             for (int i = 0; i < usernameLength; i++) {
                 String characterSet = characters[random.nextInt(2)]; // Selecting either alphabets or numbers
+
                 char randomChar = characterSet.charAt(random.nextInt(characterSet.length()));
                 email.append(randomChar);
             }
-            
-            // Adding '@' symbol
             email.append("@");
-            
+
             // Selecting random domain
             String randomDomain = domains[random.nextInt(domains.length)];
             email.append(randomDomain);
-            
+
             return email.toString();
         }
 
-        public static String generateUserName(){
+    public static String generateUserName(){
             String[] characters = {"abcdefghijklmnopqrstuvwxyz", "0123456789"};
             Random random = new Random();
             StringBuilder username = new StringBuilder();
@@ -99,6 +124,33 @@ public class TestClass1 {
              return username.toString();
         }
 
+    @Test
+    public void test11() {
+          ChromeDriver driver = new ChromeDriver();
+
+          driver.get("https://elenta.lt");
+          WebElement cookieButton = driver.findElement(By.xpath("/html/body/div[5]/div[2]/div[1]/div[2]/div[2]/button[1]/p"));
+          cookieButton.click();
+
+         WebElement login = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div/a[3]"));
+         login.click();
+
+         WebElement register = driver.findElement(By.xpath("/html/body/div[1]/form/fieldset/table/tbody/tr[10]/td/p/a"));
+         register.click();
+
+         //driver.findElement(By.id("UserName")).sendKeys();
+         driver.findElement(By.id("Email")).sendKeys(generateRandomEmail());
+         driver.findElement(By.id("Password")).sendKeys("Belekoks@23");
+         driver.findElement(By.id("Password2")).sendKeys("Belekoks@23");
+         String registruotis = new String ("/html/body/div[1]/form/fieldset/table/tbody/tr[11]/td[2]/input");
+         WebElement registration = driver.findElement(By.xpath(registruotis));
+         registration.click();
+         WebElement valifdationText = driver.findElement(By.xpath("/html/body/div[1]/div[2]/h1/b"));
+         Assert.assertEquals(valifdationText.getText(), "Jūs sėkmingai prisiregistravote!");
+
+
+
+     }
 
 
 
@@ -110,7 +162,20 @@ public class TestClass1 {
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
